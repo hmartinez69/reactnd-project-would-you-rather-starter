96c461a4881aec925c3374b3f63a0dc9a34e7fa3
hugomartinez <hugo.martinez@supdepub.com>
2020-05-12 09:34:32 +0200

    Create Login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
 import { handleInitialData } from "../actions/shared";
 import { connect } from 'react-redux';
