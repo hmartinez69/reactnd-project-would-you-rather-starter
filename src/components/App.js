fcb77c7513cbfe4fb9dd81ac00dae1bad28f574c
hugomartinez <hugo.martinez@supdepub.com>
2020-05-12 08:59:17 +0200

    Init CRA + directory

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import UITest from "./mocks/UITest1";
+
+function App() {
+  return (
+      <div className="App">
+        <UITest />
+      </div>
+  );
+}
+
+export default App;
