7a03bcbb5e5c0a2055c323ae6e734bfaf4ba58c3
hugomartinez <hugo.martinez@supdepub.com>
2020-05-12 09:15:28 +0200

    Create Reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'fomantic-ui-css/semantic.min.css'
 import './index.css'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
